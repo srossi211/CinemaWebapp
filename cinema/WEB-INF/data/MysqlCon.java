065fdd8f7b06f7c3de4b6d6eb076d719b51d0959
CBCallahan <cbc59664@uga.edu>
2018-04-19 10:26:49 -0400

    Add files via upload

---
+++ cinema/WEB-INF/data/MysqlCon.java
@@ -0,0 +1,57 @@
+package MysqlCon;
+import java.sql.*;
+import java.io.*;
+//import com.mysql.jdbc.Driver;
+
+public class MysqlCon{
+	public static Connection connect(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cinema","root","root");
+			return con;
+		} catch(Exception e) { 
+			System.out.println(e);
+		}
+		return null;
+	}
+	
+	public int insertCustomer(int customer, String email, String fname, String lname, String password, String phoneNumber, String street_address, String city, String state, String zipcode, String has_promo, String account) {
+		try{
+			Connection con = MysqlCon.connect();
+			Statement st = con.createStatement();
+			ResultSet rs;
+			int i=st.executeUpdate("insert into customer values ('"+customer+"', '"+email+"', '"+fname+"',  '"+lname+"',  '"+password+"',  '"+phoneNumber+"',  '"+street_address+"',  '"+city+"',  '"+state+"',  '"+zipcode+"',  '"+account+"',  '"+has_promo+"')");
+			return i;
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+		return 0;
+	}
+
+	public int getNextId() {
+		int max=0;
+		try{
+			Connection con = MysqlCon.connect();
+			Statement st = con.createStatement();
+			//ResultSet rs = st.executeQuery("select nvl(max(customer_id, 0) customer_id from cinema.customer");
+			//if(rs.next()){
+			//	max = rs.getInt("customer_id");
+			//}
+			max=max+1;
+			return max;
+		}catch(Exception e) {
+			System.out.println(e);
+			return -100;
+		}
+	}
+}
+
+/*  EXAMPLES
+
+Statement stmt=con.createStatement();  
+ResultSet rs=stmt.executeQuery("select * from emp");  
+while(rs.next())  
+System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+con.close();  
+
+*/
